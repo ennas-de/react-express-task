3cd964ec6bd69abee95008e204a94126b190391e
Abdulhakeem <70843998+ennas-de@users.noreply.github.com>
2022-05-22 06:49:18 +0100

    Add Controller Functions

--- backend/route/authRoutes.js
+++ backend/route/authRoutes.js
@@ -1,59 +1,15 @@
 import express from "express";
-import mongoose from "mongoose";
-import bcrypt from "bcrypt";
-import User from "../model/User.js";
+import {
+  registerController,
+  loginController,
+} from "../controller/authController.js";
 
 const router = express.Router();
 
 // Register
-router.post("/register", async (req, res) => {
-  //
-  console.log(req.body);
-  try {
-    // hashing password
-    const salt = await bcrypt.genSalt(10);
-    const hashPass = await bcrypt.hash(req.body.password, salt);
-
-    //creating new user instance
-    const ValidatedUser = new User({
-      username: req.body.username,
-      password: hashPass,
-    });
-
-    try {
-      // saving new user to db and returning a response
-      const savedUser = ValidatedUser.save();
-      savedUser && res.status(200).json("Registered Successfully!");
-    } catch (err) {
-      // if error with saving to db
-      err && res.status(401).json("Registration Failed!");
-    }
-  } catch (error) {
-    // if error with the validation
-    error && res.status(500).json(error);
-  }
-});
+router.post("/register", registerController);
 
 //Login
-router.post("/login", async (req, res) => {
-  try {
-    // console.log(req.body);
-    // first, lets find the user from the db
-    const user = await User.findOne({ username: req.body.username });
-    // if no user is found
-    !user && res.status(404).json("User Not Found!");
-    // do validations
-    const validatedUser = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
-    // if wrong password, response with an error message
-    !validatedUser && res.status(403).json("Wrong credentials!");
-
-    // remove password from the response object
-    const { password, ...others } = user._doc;
-    validatedUser && res.status(200).json(others);
-  } catch (error) {}
-});
+router.post("/login", loginController);
 
 export default router;
